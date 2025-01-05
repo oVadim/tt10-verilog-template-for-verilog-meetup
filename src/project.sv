30468d479d565b5421eb8f9d0fe192d218b403fa
Yuri Panchul <yuri@panchul.com>
2025-01-04 20:06:50 -0800

    Developing example

---
+++ src/project.sv
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2024 Tiny Tapeout and Yuri Panchul / Verilog Meetup
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_example
+(
+    input  [7:0] ui_in,    // Dedicated inputs
+    output [7:0] uo_out,   // Dedicated outputs
+    input  [7:0] uio_in,   // IOs: Input path
+    output [7:0] uio_out,  // IOs: Output path
+    output [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input        ena,      // always 1 when the design is powered, so you can ignore it
+    input        clk,      // clock
+    input        rst_n     // reset_n - low to reset
+);
+
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+  // List all unused inputs to prevent warnings
+  wire _unused = & { ena, clk, rst_n, 1'b0 };
+
+endmodule
